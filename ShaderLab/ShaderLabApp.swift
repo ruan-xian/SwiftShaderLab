9b6d1cc9d673deaf5ddcb97ae14fbf2e55296d00
Ryan Xu <ryan-xu@live.com>
2026-09-02 16:15:38 -0400

    window sizing

--- ShaderLab/ShaderLabApp.swift
+++ ShaderLab/ShaderLabApp.swift
@@ -7,23 +7,6 @@ struct ShaderLabApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView(store: store)
-                .shaderLabWindowSizing()
         }
-    }
-}
-
-private extension View {
-    @ViewBuilder
-    func shaderLabWindowSizing() -> some View {
-        #if targetEnvironment(macCatalyst)
-            frame(
-                minWidth: 1000,
-                maxWidth: .infinity,
-                minHeight: 700,
-                maxHeight: .infinity
-            )
-        #else
-            self
-        #endif
     }
 }
